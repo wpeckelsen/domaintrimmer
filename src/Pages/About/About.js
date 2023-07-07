72590a9e63cdc7f48c046aabacc0571cac8e8306
wpeckelsen <wpeckelsen@gmail.com>
2023-07-07 21:24:38 +0200

    welcome

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -16,7 +16,7 @@ function About(){
             title="About this website"
 
             content={<>
-                <p>Welcome to Domain Trimmer, a personal hobby project of Wessel Peckelsen focused on simplifying the process of cleaning lists of messy domains.</p>
+                <p>Domain Trimmer! A personal hobby project of Wessel Peckelsen focused on simplifying the process of cleaning lists of messy domains.</p>
                 <p>With this tool, you can transform messy domains into concise and visually appealing lists of URLs. </p>
                 <p>I've done my best to come up with a smart regex pattern that shaves off the mess and returns a clean result!</p>
             </>}
